f88f1701699340fd1a597a9e54256950e72b5ce3
Humberto Sandmann <hsandmann@ieee.org>
2022-10-24 12:40:52 -0300

    xls2folders

--- xls2folders.py
+++ xls2folders.py
@@ -1,6 +1,39 @@
 import pandas as pd
+import random
+import os
+from os.path import isdir, join
+
+
+path = join('suicidedb')
+
+if isdir(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(path)
+
+os.mkdir(path)
+test = join(path, 'test')
+os.mkdir(test)
+train = join(path, 'train')
+os.mkdir(train)
 
 xlsFile = pd.read_excel('tweets2019.2.xlsx', sheet_name='sheet1')
 
 for index, row in xlsFile.iterrows():
-    print(row['classe'], row['texto'])
+
+    # remover essa linha quando houver classificacao pelo especialista
+    classe = random.choice(['nocivo', 'inofensivo', 'geral'])
+    category = random.choice([train, test])
+    fpath = join(category, classe)
+    if not isdir(fpath):
+        os.mkdir(fpath)
+
+    id = row['id']
+    texto = str(row['texto'])
+
+    file = open(join(fpath, f'{str(id)}.txt'), 'w')
+    file.write(texto)
+    file.close()
